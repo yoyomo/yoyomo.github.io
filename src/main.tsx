3fd6ef8340ada9db01f84cc86e765fd22cb4a3b9
Armando Ortiz <armando.josue.og@gmail.com>
2024-06-01 16:01:55 -0700

    use vite

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { MantineProvider } from '@mantine/core'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>,
+)
