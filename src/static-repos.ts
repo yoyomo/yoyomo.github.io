5358a75a771f1dcdf75c4f2f7eaae412f46f8193
Armando Ortiz <armando.josue.og@gmail.com>
2024-06-01 18:57:26 -0700

    export directly to file

--- src/static-repos.ts
+++ src/static-repos.ts
@@ -1,16 +1,1 @@
-// This file is used to mock the response from the github api
-// locally run pnpm repos to see the list of repos
-export const repos = [
-    { "description": "Animal Crossing New Horizon Tool Usage Counter App", "has_pages": true, "name": "acnh-counter" },
-    { "description": "Checker boardgame", "has_pages": true, "name": "checker" },
-    { "description": "A circle that changes a song's tempo", "has_pages": true, "name": "circleTemper" },
-    { "description": "Apartments and houses interior architecture in three.js", "has_pages": true, "name": "house" }, // broken
-    { "description": "i ching without pen and paper", "has_pages": true, "name": "iching" },
-    { "description": "MIDI reader, parser, writer, and player", "has_pages": true, "name": "midijs" },
-    { "description": "Live example for MML JS library", "has_pages": true, "name": "mmljs-example" },
-    { "description": "Both Modular and Static Examples of MuvJS", "has_pages": true, "name": "muvjs-example" },
-    { "description": "Perimeter calculator (from 2019 gist https://gist.github.com/yoyomo/66f881cee698ee6cc7c1bc3d7ee7f8a2)", "has_pages": true, "name": "perimeter" },
-    { "description": "React + MUV architecture", "has_pages": true, "name": "react-muv-template" },
-    { "description": "sp404 utilities (Perfect Loop Start)", "has_pages": true, "name": "sp404" },
-    { "description": "Main site for all of my code/github pages", "has_pages": true, "name": "yoyomo.github.io" },
-]
+export const repos = [{ "description": "Modified Metric Time", "name": "modified-metric-time" }, { "description": "Animal Crossing New Horizon Tool Usage Counter App", "name": "acnh-counter" }, { "description": "Checker boardgame", "name": "checker" }, { "description": "A circle that changes a song's tempo", "name": "circleTemper" }, { "description": "Apartments and houses interior architecture in three.js", "name": "house" }, { "description": "i ching without pen and paper", "name": "iching" }, { "description": "MIDI reader, parser, writer, and player", "name": "midijs" }, { "description": "Live example for MML JS library", "name": "mmljs-example" }, { "description": "Both Modular and Static Examples of MuvJS", "name": "muvjs-example" }, { "description": "Perimeter calculator (from 2019 gist https://gist.github.com/yoyomo/66f881cee698ee6cc7c1bc3d7ee7f8a2)", "name": "perimeter" }, { "description": "React + MUV architecture", "name": "react-muv-template" }, { "description": "sp404 utilities (Perfect Loop Start)", "name": "sp404" }, { "description": "Main site for all of my code/github pages", "name": "yoyomo.github.io" }]
