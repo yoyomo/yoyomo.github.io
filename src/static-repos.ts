39d5d6e7b3296fdb3e7d224ac291501ff9037b94
Armando Ortiz <armando.josue.og@gmail.com>
2024-06-01 18:58:35 -0700

    comment broken links

--- src/static-repos.ts
+++ src/static-repos.ts
@@ -1,1 +1,15 @@
-export const repos = [{ "description": "Modified Metric Time", "name": "modified-metric-time" }, { "description": "Animal Crossing New Horizon Tool Usage Counter App", "name": "acnh-counter" }, { "description": "Checker boardgame", "name": "checker" }, { "description": "A circle that changes a song's tempo", "name": "circleTemper" }, { "description": "Apartments and houses interior architecture in three.js", "name": "house" }, { "description": "i ching without pen and paper", "name": "iching" }, { "description": "MIDI reader, parser, writer, and player", "name": "midijs" }, { "description": "Live example for MML JS library", "name": "mmljs-example" }, { "description": "Both Modular and Static Examples of MuvJS", "name": "muvjs-example" }, { "description": "Perimeter calculator (from 2019 gist https://gist.github.com/yoyomo/66f881cee698ee6cc7c1bc3d7ee7f8a2)", "name": "perimeter" }, { "description": "React + MUV architecture", "name": "react-muv-template" }, { "description": "sp404 utilities (Perfect Loop Start)", "name": "sp404" }, { "description": "Main site for all of my code/github pages", "name": "yoyomo.github.io" }]
+export const repos = [
+    // { "description": "Modified Metric Time", "name": "modified-metric-time" }, // 404
+    { "description": "Animal Crossing New Horizon Tool Usage Counter App", "name": "acnh-counter" },
+    { "description": "Checker boardgame", "name": "checker" },
+    { "description": "A circle that changes a song's tempo", "name": "circleTemper" },
+    // { "description": "Apartments and houses interior architecture in three.js", "name": "house" }, // broken
+    { "description": "i ching without pen and paper", "name": "iching" },
+    { "description": "MIDI reader, parser, writer, and player", "name": "midijs" },
+    { "description": "Live example for MML JS library", "name": "mmljs-example" },
+    { "description": "Both Modular and Static Examples of MuvJS", "name": "muvjs-example" },
+    { "description": "Perimeter calculator (from 2019 gist https://gist.github.com/yoyomo/66f881cee698ee6cc7c1bc3d7ee7f8a2)", "name": "perimeter" },
+    { "description": "React + MUV architecture", "name": "react-muv-template" },
+    { "description": "sp404 utilities (Perfect Loop Start)", "name": "sp404" },
+    { "description": "Main site for all of my code/github pages", "name": "yoyomo.github.io" }
+]
